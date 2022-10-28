b0771c5f09809c4aa4b1c308f2c595796ca2b695
MarcoDevelop <m.miranda2015@outlook.com>
2022-10-28 13:15:07 -0300

    Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './styles/themes/global'
 import { defaultTheme } from './styles/themes/default'
-import { Home } from './pages/Home'
+
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
 
-      <Home />
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
---
+++ src/pages/Success/index.tsx
@@ -0,0 +1,7 @@
+export function Success() {
+  return (
+    <div>
+      <h1>success</h1>
+    </div>
+  )
+}
