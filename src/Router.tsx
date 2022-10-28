a899fd7cf113315797fd9c69c969de8072415725
MarcoDevelop <m.miranda2015@outlook.com>
2022-10-28 13:28:27 -0300

    Rotas

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,13 +2,16 @@ import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Checkout } from './pages/Checkout'
 import { Success } from './pages/Success'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/checkout" element={<Checkout />} />
-      <Route path="/success" element={<Success />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/success" element={<Success />} />
+      </Route>
     </Routes>
   )
 }
--- src/styles/themes/global.ts
+++ src/styles/themes/global.ts
@@ -14,7 +14,7 @@ export const GlobalStyle = createGlobalStyle`
 
   body {
     background: ${(props) => props.theme.background};
-    color: ${(props) => props.theme['gray-100']};
+    color: ${(props) => props.theme['gray-900']};
     -webkit-font-smoothing: antialiased;
   }
 
