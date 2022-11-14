d54b234dd3694be7988c05edca9f4708c9a875eb
MarcoDevelop <m.miranda2015@outlook.com>
2022-11-14 14:45:27 -0300

    Section Title

--- src/components/CheckoutForm/index.tsx
+++ src/components/CheckoutForm/index.tsx
@@ -1,13 +1,24 @@
 import { TitleText } from '../../styles/themes/global'
+import { SectionTitle } from '../SectionTitle'
 import { CheckoutFormContainer, FormSectionContainer } from './styles'
+import { MapPinLine } from 'phosphor-react'
+import { useTheme } from 'styled-components'
 
 export function CheckoutForm() {
+  const { colors } = useTheme()
+
   return (
     <CheckoutFormContainer>
       <TitleText size="xs" color="subtitle">
         Complete seu pedido
       </TitleText>
-      <FormSectionContainer></FormSectionContainer>
+      <FormSectionContainer>
+        <SectionTitle
+          title="Endereço de Entrega"
+          subtitle="Informe o endereço onde deseja receber seu pedido"
+          icon={<MapPinLine color={colors['brand-yellow-dark']} size={22} />}
+        />
+      </FormSectionContainer>
     </CheckoutFormContainer>
   )
 }
