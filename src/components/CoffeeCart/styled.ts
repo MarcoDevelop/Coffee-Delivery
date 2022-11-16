01b4daff370fa6e6bd38e88ec936c11b53c2a60a
MarcoDevelop <m.miranda2015@outlook.com>
2022-11-16 14:14:02 -0300

    Coffee card

---
+++ src/components/CoffeeCart/styled.ts
@@ -0,0 +1,51 @@
+import styled from 'styled-components'
+
+export const CoffeeCartContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  border-bottom: 1px solid ${({ theme }) => theme.colors['base-button']};
+  padding-bottom: 1.5rem;
+  margin-bottom: 1.5rem;
+
+  > div {
+    display: flex;
+    align-items: center;
+    gap: 1.25rem;
+
+    img {
+      width: 4rem;
+      height: 4rem;
+    }
+  }
+
+  > p {
+    align-self: flex-start;
+    font-weight: 700;
+  }
+`
+
+export const ActionsContainer = styled.div`
+  margin-top: 0.5rem;
+  height: 2rem;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  > div {
+    max-width: 4.5rem;
+    height: 100%;
+  }
+`
+
+export const RemoveButton = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 0.25rem;
+  color: ${({ theme }) => theme.colors['base-text']};
+  height: 100%;
+  border: none;
+  background: ;
+`
