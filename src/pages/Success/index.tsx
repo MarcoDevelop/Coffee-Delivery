82193fa0ba3a029a74996cb25415f9d395122a59
MarcoDevelop <m.miranda2015@outlook.com>
2022-11-17 15:07:04 -0300

    Page Success Done

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,7 +1,57 @@
+import { RegularText, TitleText } from '../../styles/themes/global'
+import { OrderDetailsContainer, OrderSuccess } from './styles'
+import Illustration from '../../assets/Illustration.png'
+import { MapPin, Clock, CurrencyDollar } from 'phosphor-react'
+import { IconsType } from '../../components/IconsType'
+import { useTheme } from 'styled-components'
+
 export function Success() {
+  const { colors } = useTheme()
   return (
     <div>
-      <h1>success</h1>
+      <OrderSuccess className="container">
+        <div>
+          <TitleText size="l">Uhu! Pedido confirmado</TitleText>
+          <RegularText>
+            Agora é só aguardar que logo o café chegará até você
+          </RegularText>
+        </div>
+
+        <section>
+          <OrderDetailsContainer>
+            <IconsType
+              icon={<MapPin weight="fill" />}
+              iconBg={colors['brand-purple']}
+              text={
+                <RegularText>
+                  Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
+                  <br />
+                  Farrapos - Porto Alegre, RS
+                </RegularText>
+              }
+            />
+            <IconsType
+              icon={<Clock weight="fill" />}
+              iconBg={colors['brand-yellow']}
+              text={
+                <RegularText>
+                  Previsão de entrega <strong>20 min - 30 min</strong>
+                </RegularText>
+              }
+            />
+            <IconsType
+              icon={<CurrencyDollar weight="fill" />}
+              iconBg={colors['brand-yellow-dark']}
+              text={
+                <RegularText>
+                  Pagamento na entrega <strong>Cartão de Crédito</strong>
+                </RegularText>
+              }
+            />
+          </OrderDetailsContainer>
+          <img src={Illustration} alt="" />
+        </section>
+      </OrderSuccess>
     </div>
   )
 }
