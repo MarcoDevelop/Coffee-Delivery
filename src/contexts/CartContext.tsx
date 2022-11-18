39b0184b49d83e6200f803c9e35bfc32eaf17116
MarcoDevelop <m.miranda2015@outlook.com>
2022-11-18 12:48:14 -0300

    Function add cart

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useState } from 'react'
 import { Coffee } from '../components/CoffeeCard'
+import { produce } from 'immer'
 
 export interface CartItem extends Coffee {
   quantity: number
@@ -17,6 +18,22 @@ export const CardContext = createContext({} as CardContextType)
 
 export function CartContextProvider({ children }: CardContextProviderProps) {
   const [cartItems, setCartItems] = useState<CartItem[]>([])
+
+  function addCoffeeToCart(coffee: CartItem) {
+    const coffeeAlreadyExistsInCart = cartItems.findIndex(
+      (cartItem) => cartItem.id === coffee.id,
+    )
+
+    const newCart = produce(cartItems, (draft) => {
+      if (coffeeAlreadyExistsInCart < 0) {
+        draft.push(coffee)
+      } else {
+        draft[coffeeAlreadyExistsInCart].quantity += coffee.quantity
+      }
+    })
+
+    setCartItems(newCart)
+  }
 
   return (
     <CardContext.Provider value={{ cartItems }}>
