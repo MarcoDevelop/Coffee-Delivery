802340f5ec50c99ec8c5e066574573bb0bda5f14
MarcoDevelop <m.miranda2015@outlook.com>
2022-11-22 17:06:51 -0300

    Zod

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,12 +1,38 @@
 import { CheckoutForm } from '../../components/CheckoutForm'
 import { CoffeeSelected } from '../../components/CoffeeSelected'
 import { CheckoutContainer } from './styles'
+import * as zod from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm, FormProvider } from 'react-hook-form'
+
+const confirmOrderFormValidationSchema = zod.object({
+  cep: zod.string(),
+})
+
+export type OrderData = zod.infer<typeof confirmOrderFormValidationSchema>
+
+type ConfirmOrderFormData = OrderData
 
 export function Checkout() {
+  const confirmOrderForm = useForm<ConfirmOrderFormData>({
+    resolver: zodResolver(confirmOrderFormValidationSchema),
+  })
+
+  const { handleSubmit } = confirmOrderForm
+
+  function handleConfirmOrder(data: ConfirmOrderFormData) {
+    console.log(data)
+  }
+
   return (
-    <CheckoutContainer className="container">
-      <CheckoutForm />
-      <CoffeeSelected />
-    </CheckoutContainer>
+    <FormProvider {...confirmOrderForm}>
+      <CheckoutContainer
+        className="container"
+        onSubmit={handleSubmit(handleConfirmOrder)}
+      >
+        <CheckoutForm />
+        <CoffeeSelected />
+      </CheckoutContainer>
+    </FormProvider>
   )
 }
