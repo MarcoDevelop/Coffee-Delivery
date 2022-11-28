5726f406588b22c9d240914a093d690ff4be0d03
MarcoDevelop <m.miranda2015@outlook.com>
2022-11-28 19:49:01 -0300

    Done Project

--- src/components/CheckoutForm/PaymentInput/index.tsx
+++ src/components/CheckoutForm/PaymentInput/index.tsx
@@ -12,7 +12,7 @@ export const PaymentMethodInput = forwardRef<
 >(({ id, icon, label, ...props }, ref) => {
   return (
     <PaymentMethodContainer>
-      <input id={id} type="radio" {...props} name="paymentMethods" ref={ref} />
+      <input id={id} type="radio" {...props} name="paymentMethod" ref={ref} />
       <label htmlFor={id}>
         <ContentContainer>
           {icon}
